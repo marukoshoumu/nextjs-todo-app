60f85f4472f2790f49af521302d1e858b32230e5
maruko <g031g143@gmail.com>
2024-11-05 22:30:11 +0900

    新規登録

---
+++ app/head.tsx
@@ -0,0 +1,13 @@
+// ヘッダー
+const Head = () => {
+  return (
+    <>
+      <title>Todo</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Next.js Supabase Todo" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+};
+
+export default Head;
